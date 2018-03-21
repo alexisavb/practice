8f24db22145514c370e6cb4755aa8596b2562793
Alexis Osnaya <alexis.osnaya.ipn@gmail.com>
2018-03-20 22:25:17 -0600

    success 1_2

--- array and string/1_2.cpp
+++ array and string/1_2.cpp
@@ -9,27 +9,21 @@ class Permutation{
 
 bool Permutation::isPermutation(string str1, string str2){
 	int sizeStr1 = str1.length();
-	int sizeStr2 = str2.length();
-	if(str1 != str2){
+	int sizeStr2 = str2.length();	
+	if(sizeStr1 != sizeStr1){		
 		return false;
 	}
 
 	int *array = new int[128]();
 
-	for(char &c : str1){
+	for(char &c : str1){		
 		array[c]++;
 	}
 
 	for(char &c : str2){
 		array[c]--;
 		if(array[c] < 0){return false;}	
-	}
-
-
-	cout <<  "sumaa ";
-	for(int i = 0; i < 128;i++){
-		cout << array[i] << " ";
-	}
+	}		
 	return true;
 }
 
@@ -37,7 +31,7 @@ bool Permutation::isPermutation(string str1, string str2){
 int main(){
 	Permutation per;
 	string s1 = "hola";
-	string s2 = "aloh";
+	string s2 = "alhh";
 	if(per.isPermutation(s1,s2)){
 		cout << "yes" << endl;
 	}else{
